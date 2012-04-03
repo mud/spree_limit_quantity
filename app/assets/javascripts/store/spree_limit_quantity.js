3afc9584718e69f4b8594a2d7a5b096449c45882
Takashi Okamoto <mud@media.mit.edu>
2012-04-03 10:44:01 -0400

    Added JS alert when adding more than available.

--- app/assets/javascripts/store/spree_limit_quantity.js
+++ app/assets/javascripts/store/spree_limit_quantity.js
@@ -1,1 +1,11 @@
-//= require store/spree_core
+(function($) {
+  $(function() {
+    $('input[type="number"]').on('blur', function() {
+      var $this = $(this);
+      if ($this.attr('max') && parseInt($this.attr('max')) < $this.attr('value')) {
+        $this.attr('value', $this.attr('max'));
+        alert('There are only ' + $this.attr('max') + ' items available for this product.');
+      }
+    });
+  });
+})(jQuery);
