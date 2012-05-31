92696a5503876a615c22cb0ccb19597328d28054
Takashi Okamoto <mud@media.mit.edu>
2012-05-31 10:39:26 -0400

    Updated to Spree 1.1.1.

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,6 +1,7 @@
 Spree::Product.class_eval do
   has_one :quantity_limit, :class_name => 'Spree::QuantityLimit'
   accepts_nested_attributes_for :quantity_limit
+  attr_accessible :quantity_limit_attributes
   
   def quantity_limited?
     !quantity_limit.nil? && quantity_limit[:limit] > 0
